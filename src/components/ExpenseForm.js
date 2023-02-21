3c4c517d7eea5bf28fc0af6518f856eaaddd434c
Jay Rexiti <jrexiti@totalwine.com>
2023-02-21 16:10:49 -0500

    changes to form

--- src/components/ExpenseForm.js
+++ src/components/ExpenseForm.js
@@ -6,41 +6,42 @@ const ExpenseForm = ({
   amount,
   handleCharge,
   handleAmount,
-  HandleSubmit,
+  handleSubmit,
 }) => {
-  return;
-  <form>
-    <div className="form-center">
-      <div className="form-group">
-        <label htmlFor="charge">charge</label>
-        <input
-          type="text"
-          className="form-control"
-          id="charge"
-          name="charge"
-          placeholder="example rent"
-          value={charge}
-          onChange={handleCharge}
-        />
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="form-center">
+        <div className="form-group">
+          <label htmlFor="charge">charge</label>
+          <input
+            type="text"
+            className="form-control"
+            id="charge"
+            name="charge"
+            placeholder="example rent"
+            value={charge}
+            onChange={handleCharge}
+          />
+        </div>
+
+        <div className="form-group">
+          <label htmlFor="amount">amount</label>
+          <input
+            type="number"
+            className="form-control"
+            id="amount"
+            name="amount"
+            placeholder="example 100"
+            value={amount}
+            onChange={handleAmount}
+          />
+        </div>
       </div>
-    </div>
-    <div className="form-center">
-      <div className="form-group">
-        <label htmlFor="amount">amount</label>
-        <input
-          type="number"
-          className="form-control"
-          id="amount"
-          name="amount"
-          placeholder="example 100"
-          value={amount}
-        />
-      </div>
-    </div>
-    <button type="submit" className="btm">
-      Submit <MdSend className="btn-icon" />
-    </button>
-  </form>;
+      <button type="submit" className="btn">
+        Submit <MdSend className="btn-icon" />
+      </button>
+    </form>
+  );
 };
 
 export default ExpenseForm;
